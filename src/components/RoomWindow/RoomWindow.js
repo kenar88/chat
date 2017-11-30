8b3fd8459c813b0cc9b12dae58c77426b6a9ab9f
artrnh <artrnh@gmail.com>
2017-11-30 19:16:58 +0300

    pull request test #2

--- src/components/RoomWindow/RoomWindow.js
+++ src/components/RoomWindow/RoomWindow.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { MessageForm } from '../MessageForm/MessageForm';
+import { Message } from '../Message/Message';
 
 import './RoomWindow.css';
 
@@ -10,6 +11,9 @@ export default class RoomsWindow extends React.Component {
       <section className="RoomWindow">
         <h2 className="RoomWindow__title">{this.props.name}</h2>
         <h2 className="RoomWindow__title">{`Room ID: ${this.props.match.params.id}`}</h2>
+        <Message text="Test" />
+        <Message text="Test" />
+        <Message text="Test" />
         <MessageForm />
       </section>
     );
